832779c53095a90aa93e15e2a1fd4a31e8efedc4
Amigo Solidário <168089592+wesleyouza98@users.noreply.github.com>
2025-05-27 19:19:59 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'energia-quantica-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
