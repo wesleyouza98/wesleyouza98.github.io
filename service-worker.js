1bb1905098da75e5712cf15fd56bafb7bf71d368
Amigo Solidário <168089592+wesleyouza98@users.noreply.github.com>
2025-05-28 10:52:24 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -27,6 +27,20 @@ self.addEventListener("activate", (event) => {
           }
         })
       )
+                       <script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(error => {
+          console.log('Falha ao registrar o Service Worker:', error);
+        });
+    });
+  }
+</script>
+
     )
   );
 });
